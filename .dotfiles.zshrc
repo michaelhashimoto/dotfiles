c842087c975d000ee58ac0f97ab6b7e912109ef7
Michael Hashimoto <michael.hashimoto@liferay.com>
2025-06-30 10:36:21 -0700

    Add basic configurations for lazygit

---
+++ .dotfiles.zshrc
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+alias lazygit="lazygit --use-config-dir ~/.config/lazygit"
--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "cp -f .dotfiles.zshrc ~/.dotfiles.zshrc"
+
+cp -f .dotfiles.zshrc ~/.dotfiles.zshrc
+
+grep -qxF 'source ~/.dotfiles.zshrc' ~/.zshrc || echo 'source ~/.dotfiles.zshrc' >> ~/.zshrc
+
 for config_file in $(ls .config/)
 do
-	echo config_file=${config_file}
-
 	echo "rm -rf ~/.config/${config_file}"
 
 	rm -rf ~/.config/${config_file}
