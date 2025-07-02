d2f87a7143b2bd92b4a93ab76102ac02dbe07428
Michael Hashimoto <michael.hashimoto@liferay.com>
2025-07-02 14:27:29 -0700

    Add catppuccin theme to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,3 +12,39 @@ vim.keymap.set("n", "<leader>y", [["+Y]])
 vim.opt.fixeol = false
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+--    { "nvim-telescope/telescope.nvim", tag = "0.1.8",
+--        dependencies = {
+--            "nvim-lua/plenary.nvim"
+--        }
+--    }
+}
+
+require("lazy").setup(plugins, opts)
+
+require("catppuccin").setup()
+
+vim.cmd.colorscheme "catppuccin"
+
+-- local builtin = require('telescope.builtin')
+
+-- vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
