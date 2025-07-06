d814988246da3d546072984ba2538c6703788aab
Michael Hashimoto <michael.hashimoto@liferay.com>
2025-07-06 16:51:30 -0700

    Add treesitter to fold & open code blocks in lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,26 +16,27 @@ vim.opt.relativenumber = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
 local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { "nvim-telescope/telescope.nvim", tag = "0.1.8",
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        }
-    }
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		}
+	},
+	{ "nvim-treesitter/nvim-treesitter", build= ":TSUpdate" },
 }
 
 require("lazy").setup(plugins, opts)
@@ -48,3 +49,17 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {"lua", "javascript"},
+	highlight = { enable = true },
+	indent = { enable = true },
+})
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Optional: Start with all folds open
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
